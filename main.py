0610ec7a8bace43cccc5c15ede7f0fe554edc5bf
ddawkinsIntel <darryl.dawkins@intel.com>
2022-02-14 19:12:41 -0700

    - resttructured for ui integration - added drop down menu options - added s.tracker capacity lines

--- main.py
+++ main.py
@@ -4,39 +4,17 @@ import numpy.random.entropy
 import tkinter as tk
 from tkinter import messagebox
 import os
-import pathlib
 import database_funtions as db_func
 import graph_write_functions as gw_func
+import xlwings as xw
+import pandas as pd
 
 ########################################################################################################################
 
-# Begins to make the space graph workbook
-def main_graph_func(id_version, id_wif, conn):
 
-    space_alloc_table, bldg_space_table, version_name = db_func.get_db_data(conn, id_version, id_wif)
-
-    # Set excel file name
-    excel_file_name = r"pySpaceGraph v" + id_version + "w" + id_wif + "_" +version_name + ".xlsx"
-
-    # Check if file exists
-    folder = r".\graphs\\"
-
-    # Create graph directory if it does not exist
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    file_path = folder + excel_file_name
-    file = pathlib.Path(file_path)
-    if file.exists():
-        print("File exist")
-        gw_func.edit_space_graphs(space_alloc_table, bldg_space_table, file_path)
-
-    else:
-        print("File not exist")
-        gw_func.create_space_graph_wb(file_path, space_alloc_table, bldg_space_table)
 
 # After values are verified, this fires up the program
-def fire_up(id_version, id_wif):
+def fire_up(id_version, id_wif, s_trk_wb, s_trk_sheet):
     # Convert id_version and id_wif to string
     id_version = str(id_version)
     id_wif = str(id_wif)
@@ -44,18 +22,26 @@ def fire_up(id_version, id_wif):
     # Intiates connection to the sql server
     cnxn = db_func.initiate_conn()
 
+    # Get values from "wb_from" & set as capacity_sheet
+    s_trk_wb = xw.books[s_trk_wb]
+    node_sheet = s_trk_wb.sheets[s_trk_sheet]
+    node_rollup_df = node_sheet.range('A1').options(pd.DataFrame, index=False, expand='table').value
+
     # Make the space graph workbook
-    main_graph_func(id_version, id_wif, cnxn)
+    gw_func.main_graph_func(id_version, id_wif, cnxn, node_rollup_df)
 
     current_directory = os.getcwd()
     success_msg = "Graphs are located in the following folder.... \n \n" + current_directory
     messagebox.showinfo("Success!", success_msg)
 
-# Gets and checks value user enters
+# First function to be called gets and checks value user enters
 def get_value():
     # Gets values from each field
     version_value = version_entry.get()
     wif_value = wif_entry.get()
+
+    scr_trk_wb = scr_trk_workbook.get()
+    scr_trk_sheet = scr_trk_sheet_entry.get()
 
     try:
         int(version_value) # checks if version_value is an int
@@ -69,7 +55,7 @@ def get_value():
         print("version value is: ", version_value)
         print("wif value is: ", wif_value)
 
-        fire_up(version_value, wif_value)
+        # fire_up(version_value, wif_value, scr_trk_wb, scr_trk_sheet)
 
     except ValueError:
         messagebox.showerror("Error", "Value Must Be a Number")
@@ -88,6 +74,8 @@ def get_value():
     # int(version_value)
     # print(version_value)
     # fire_up(version_value)
+    fire_up(version_value, wif_value, scr_trk_wb, scr_trk_sheet)
+
 
 
 # Terinates program when user closes the window
@@ -103,10 +91,23 @@ root = tk.Tk()
 # Initiate versionValue and wifValue
 versionValue = tk.StringVar()
 wifValue = tk.StringVar()
+scr_trk_workbook = tk.StringVar()
+scr_trk_sheet = tk.StringVar()
 
 # Window title
-root.title('Space Model Graphs')
-root.geometry('325x165') # window size
+root.title('Space Model Graphing App')
+root.geometry('550x300') # window size
+
+book_name_list = []
+for i in range(len(xw.books)):
+    book_name_list.append(xw.books[i].name)
+
+# Workbook dropdown menu options
+wb_options = book_name_list
+
+# Initial Workbook dropdown menu text
+scr_trk_workbook.set(book_name_list[0])
+
 
 # Creates version label and entry field 
 version_label = tk.Label(root, text="Enter Version ID:").grid(row=1, column=1, sticky=tk.W, pady=15)
@@ -118,12 +119,23 @@ wif_label = tk.Label(root, text="Enter WIF ID:").grid(row=2, column=1, sticky=tk
 wif_entry = tk.Entry(root, width=30, textvariable=wifValue)
 wif_entry.grid(row=2, column=2, sticky=tk.W, pady=15)
 
+# Create "Cap Workbook" Dropdown menu
+scr_trk_workbook_label = tk.Label(root, text="S.Tracker Workbook:").grid(row=3, column=1, sticky=tk.W, pady=15)
+scr_trk_workbook_op = tk.OptionMenu( root , scr_trk_workbook , *wb_options )
+scr_trk_workbook_op.grid(row=3, column=2, sticky=tk.W, pady=15)
+
+# Creates wif label and entry field 
+scr_trk_sheet_label = tk.Label(root, text="S.Tracker Sheet Name:").grid(row=4, column=1, sticky=tk.W, pady=15)
+scr_trk_sheet_entry = tk.Entry(root, width=30, textvariable=scr_trk_sheet)
+scr_trk_sheet_entry.grid(row=4, column=2, sticky=tk.W, pady=15)
+
 # Creates "submit" button and calls get_value function when clicked
-submit_btn = tk.Button(root, text="Submit", width=10, command=get_value).grid(row=3, column=1, columnspan=5, pady=25)
+submit_btn = tk.Button(root, text="Submit", width=10, command=get_value).grid(row=5, column=1, columnspan=5, pady=25)
 
 # Calls get_value() function when the user hits the "enter" key while in the version_entry or wif_entry field
 version_entry.bind("<Return>", lambda e: get_value())
 wif_entry.bind("<Return>", lambda e: get_value())
+scr_trk_sheet_entry.bind("<Return>", lambda e: get_value())
 
 # Quits program on close
 root.protocol("WM_DELETE_WINDOW", on_closing)
