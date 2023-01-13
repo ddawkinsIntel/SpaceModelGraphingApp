fdfd88ed0f53e958a974f796651ea1da73b4c447
ddawkinsIntel <darryl.dawkins@intel.com>
2023-01-12 21:11:54 -0700

    - modified comments - muted warnings

--- primary_functions.py
+++ primary_functions.py
@@ -2,6 +2,8 @@ import os
 import pathlib
 import database_funtions as db_func
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
 import create_space_functions as csf
 import edit_space_functions as esf
 import universal_functions as uf
@@ -15,15 +17,17 @@ def main_graph_func(id_version, id_wif, conn, node_sheet):
 
     # Set excel file name
     excel_file_name = f'{wif_name}w{id_wif}v{id_version}_{version_name}.xlsx'
-    # Check if file exists
+
+    # folder location for the graph output
     folder = '.\graphs\\'
 
-    # Create graph directory if it does not exist
+    # Create the 'graphs' folder, if it does not exist
     if not os.path.exists(folder):
         os.makedirs(folder)
 
+    # Creating the 'graphs' folders file path
     file_path = folder + excel_file_name
-    file = pathlib.Path(file_path)
+    file = pathlib.Path(file_path) # creates an concrete path for the platform the code is running on
 
     # Check if node_sheet exists, if so get data
     if node_sheet is None:
@@ -33,22 +37,25 @@ def main_graph_func(id_version, id_wif, conn, node_sheet):
         node_rollup_df = node_sheet.range('A1').options(pd.DataFrame, index=False, expand='table').value
         node_rollup_df = uf.format_node_rollup(node_rollup_df)
 
-    # Check if file exists
+    # Check if file exists and edits the file, else creates new file
     if file.exists():
         print("File exist")
 
+        # Intiate an 'xw' object that contains all the active(open) excel workbooks
         active_xl_books = xw.books
         book_name_list = []
+
+        # Loop through active excel workbooks and adds there name to the book_name_list
         for i in range(len(active_xl_books)):
             book_name_list.append(active_xl_books[i].name)
 
-        # Check if file is open
+        # Check if the intended file is active by checking if it's in the book_name_list 
         if excel_file_name in book_name_list:
-            wb = xw.Book(excel_file_name)
+            wb = xw.Book(excel_file_name) # stores value of the active sheet, since it's active file_path not needed
             print("used book name")
         else:
             # write df to excel sheet
-            wb = xw.books.open(file_path)
+            wb = xw.books.open(file_path) # stores path to the intended file
             print("used file path")
 
         esf.edit_space_graphs(space_alloc_table, bldg_space_table, wb, node_rollup_df)
