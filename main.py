c10a2f37abf8a27023734c6e25bb02e8120ca06c
ddawkinsIntel <darryl.dawkins@intel.com>
2023-01-12 21:11:29 -0700

    - removed zbb'd scenario tracker code

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ import xlwings as xw
 
 
 # After values are verified, this starts the program
-def fire_up(id_version, id_wif, s_trk_wb, s_trk_sheet):
-    # Convert id_version and id_wif to string
+def fire_up(id_version, id_wif):
+
+    # Convert id_version and id_wif to string values
     id_version = str(id_version)
     id_wif = str(id_wif)
 
@@ -22,20 +23,7 @@ def fire_up(id_version, id_wif, s_trk_wb, s_trk_sheet):
     cnxn = db_func.initiate_conn()
     pf.main_graph_func(id_version, id_wif, cnxn, None)
 
-    # ZBB'd - was used for pulling in the scenario tracker data
-    # if s_trk_wb == "None":
-    #     node_sheet = None
-    #     pf.main_graph_func(id_version, id_wif, cnxn, node_sheet)
-
-    # else:
-    #     # Get values from "wb_from" & set as capacity_sheet
-    #     s_trk_wb = xw.books[s_trk_wb]
-    #     node_sheet = s_trk_wb.sheets[s_trk_sheet]
-
-    #     # Make the space graph workbook
-    #     pf.main_graph_func(id_version, id_wif, cnxn, node_sheet)
-
-
+    # Gets file path of the app's current location 
     current_directory = os.getcwd()
     success_msg = "Graphs are located in the following folder.... \n \n" + current_directory
     messagebox.showinfo("Success!", success_msg)
@@ -45,10 +33,6 @@ def get_value():
     # Gets values from each field
     version_value = version_entry.get()
     wif_value = wif_entry.get()
-
-    scr_trk_wb = scr_trk_workbook.get()
-    # scr_trk_sheet = scr_trk_sheet_entry.get()
-    scr_trk_sheet = "Node rollups"
 
     try:
         int(version_value) # checks if version_value is an int
@@ -62,7 +46,8 @@ def get_value():
         print("version value is: ", version_value)
         print("wif value is: ", wif_value)
 
-        fire_up(version_value, wif_value, scr_trk_wb, scr_trk_sheet)
+        # starts the application logic
+        fire_up(version_value, wif_value)
 
     except ValueError:
         messagebox.showerror("Error", "Value Must Be a Number")
@@ -84,10 +69,10 @@ def get_value():
     #     int(wif_value)
     # print(version_value)
     # print(wif_value)
-    # fire_up(version_value, wif_value, scr_trk_wb, scr_trk_sheet)
+    # fire_up(version_value, wif_value)
 
 
-# Terinates program when user closes the window
+# Terminates program when user closes the window
 def on_closing():
     root.destroy()
     quit()
@@ -107,17 +92,6 @@ scr_trk_sheet = tk.StringVar()
 root.title('Space Model Graphing App')
 root.geometry('350x175') # window size
 
-# ZBB'd - was used for pulling in the scenario tracker data
-# book_name_list = ["None"]
-# for i in range(len(xw.books)):
-#     book_name_list.append(xw.books[i].name)
-
-# # Workbook dropdown menu options
-# wb_options = book_name_list
-
-# # Initial Workbook dropdown menu text
-# scr_trk_workbook.set(book_name_list[0])
-
 
 # Creates version label and entry field 
 version_label = tk.Label(root, text="Enter Version ID:").grid(row=1, column=1, sticky=tk.W, pady=15)
@@ -128,17 +102,6 @@ version_entry.grid(row=1, column=2, sticky=tk.W, pady=15)
 wif_label = tk.Label(root, text="Enter WIF ID:").grid(row=2, column=1, sticky=tk.W, pady=15)
 wif_entry = tk.Entry(root, width=30, textvariable=wifValue)
 wif_entry.grid(row=2, column=2, sticky=tk.W, pady=15)
-
-# Was using the below chunck of code to pullin a scenario track to eventually add a visual that includes the capacity bar garphs
-# # Create "Cap Workbook" Dropdown menu
-# scr_trk_workbook_label = tk.Label(root, text="S.Tracker Workbook:").grid(row=3, column=1, sticky=tk.W, pady=15)
-# scr_trk_workbook_op = tk.OptionMenu( root , scr_trk_workbook , *wb_options )
-# scr_trk_workbook_op.grid(row=3, column=2, sticky=tk.W, pady=15)
-
-# # Creates wif label and entry field 
-# scr_trk_sheet_label = tk.Label(root, text="S.Tracker Sheet Name:").grid(row=4, column=1, sticky=tk.W, pady=15)
-# scr_trk_sheet_entry = tk.Entry(root, width=30, textvariable=scr_trk_sheet)
-# scr_trk_sheet_entry.grid(row=4, column=2, sticky=tk.W, pady=15)
 
 # Creates "submit" button and calls get_value function when clicked
 submit_btn = tk.Button(root, text="Submit", width=10, command=get_value).grid(row=5, column=1, columnspan=5, pady=25)
